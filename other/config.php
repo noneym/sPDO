e4fa6cbc9b43ea64b5d1e1f914566c9f44d26760
Gürkan Biçer <gurkanbicer@yandex.com>
2013-10-25 13:47:13 +0300

    Create config.php

---
+++ other/config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Gürkan Biçer <gurkanbicer@yandex.com>
+ *
+ * @for sPDO
+ */
+
+$sPDO_cnf = array(
+
+    /**
+     * MySQL kullanıcı bilgileri
+     *
+     */
+    'username'      => '',
+    'password'      => '',
+
+    /**
+     * MySQL veritabanı ve host adı
+     *
+     */
+    'database'      => '',
+    'hostname'      => 'localhost',
+
+    /**
+     * MySQL bağlantısı için karakter seti
+     *
+     */
+    'char_set'      => 'utf8',
+
+    /**
+     * SQL sorgu sonuçlarının önbelleklenmesi
+     *
+     */
+    'cache'         => true,
+    'cachedir'      => 'cache/',
+    'cacheexpire'   => 900,
+
+);
+
+/* End of file config.php */
